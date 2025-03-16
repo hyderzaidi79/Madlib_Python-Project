eb2c85472b52f289212186c325c7dce931474c6b
hyderzaidi79 <muhammadhyderpk@gmail.com>
2025-03-16 14:06:31 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+Plular_Noun = input("Enter a Plural noun: ")
+Occupation = input("Enter an Occupation: ")
+A_Place = input("Enter a Place: ")
+Number = input("Enter a Number: ")
+Adjective = input("Enter an Adjective: ")
+Verb_ing = input("Enter a Verb ending with 'ing': ")
+Noun = input("Enter a Noun: ")
+Part_of_Body = input("Enter a Part of Body: ")
+Verb = input("Enter a Verb: ")
+
+madlib = f"""Hello, my fellow {Plular_Noun} in 2020, it me, George Washingron, the first {Occupation}.I am writing from 
+(the) {A_Place} where I have been secretly living for the past {Number} years. I am concerned by the {Adjective} state 
+of affairs in America these days. It seems that your politicians are more concerned with {Verb_ing} one another than with
+listening to the {Plular_Noun} of the people. When we declared our independence from (the) {A_Place}, we set forth on 
+a/an {Adjective} path guided by the voices of the everyday {Plular_Noun}. If we're going to keep {Verb_ing}, then we need
+to learn how to respect all {Plular_Noun}. Don't get me wrong; we had {Adjective} problems in my day, too. Benjamin 
+Franklin once called me a/an {Noun} and kicked me in the {Part_of_Body}. But at the end of the day, we were able to {Verb}
+in harmony. Let us find that {Adjective} spirit once again, or else I'm taking my {Part_of_Body} off the quarter!"""
+
+print(madlib)
